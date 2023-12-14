c86f91e0b2f5fe2c59bd1f79947b0fb74225b0ae
Dhruv_Anand <ananddhruv475@gmail.com>
2023-12-14 20:19:06 +0530

    completed

--- index.js
+++ index.js
@@ -7,27 +7,37 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(cors());
+
+// Update the origin to match your deployed React app's URL on Vercel
+const allowedOrigin = 'https://product-manager-one.vercel.app';
+
+app.use(cors({
+  origin: allowedOrigin,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+  allowedHeaders: 'Content-Type,Authorization',
+}));
+
 const shopifyApiKey = '20413f9029c695a6bb194d66f1b34b82';
 const shopifyApiSecret = '45b70d9bae602557f168f0120ca9cdb5';
 const shopifyStore = 'made2automate';
 const adminApiAccessToken = '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';
 
 // Endpoint to add a product
-
 app.get('/ping', (req, res) => {
-  
-  res.send("server pinged!!")
-})
+  res.send("server pinged!!");
+});
 
 app.post('/add-product', async (req, res) => {
   try {
     const productData = req.body;
+    
 
     // Make a request to Shopify API to add the product
     const response = await axios.post(
       `https://${shopifyStore}.myshopify.com/admin/api/2021-07/products.json`,
-      { product: productData },
+      { product: productData.product },
       {
         headers: {
           'X-Shopify-Access-Token': adminApiAccessToken,
